f7a8aec2f5d1cd35e26093dd4256ef5342886410
YOSHIDA Masanori <masanori.yoshida@gmail.com>
2024-04-05 03:55:31 +0900

    add hardhat task "deploy"
    Signed-off-by: YOSHIDA Masanori <masanori.yoshida@gmail.com>

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -4,3 +4,29 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.24",
 };
+
+async function deploy(contractName) {
+  const factory = await hre.ethers.getContractFactory(contractName);
+  const contract = await factory.deploy();
+  await contract.waitForDeployment();
+  return contract;
+}
+
+async function saveAddress(contract) {
+  const fs = require("fs");
+  fs.writeFileSync("address.txt", contract.target);
+}
+
+task("deploy", "deploy contracts", async (taskArgs, hre) => {
+  let contract, prev;
+  for (let i = 0; i < 3; i++) {
+    contract = await deploy("C");
+    if (prev) {
+      const tx = await contract.setCallee(prev.target);
+      await tx.wait();
+    }
+    prev = contract;
+  }
+
+  await saveAddress(contract);
+});
